eec34d69574e937260f3afe14e51c86a32fbda39
Vinicius Zorzi Maino <vzmaino@hotmail.com>
2021-01-28 23:02:26 -0300

    Initial Commit

---
+++ CryptoPrice/CryptoPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceApp.swift
+//  CryptoPrice
+//
+//  Created by Vinicius Maino on 28/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
